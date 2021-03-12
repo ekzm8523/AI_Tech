ad04996f3a09d4bc2fb2888d86e8c892e959a110
JaewonMin <mjw8523@gmail.com>
2021-03-12 22:19:02 +0900

    3/12

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import torch
 
-x = torch.randn(2, requires_grad = False)
-y = x * 3
-gradients = torch.tensor([100,0.1], dtype=torch.float)
-y.backward(gradients)
-print(x.grad)
+x = torch.ones(2,2, requires_grad = True)
+y = x + 2
+
+z = y*y*3
+
+out = z.mean()
+print(out)
+out.backward()	# out.backward(torch.tensor(1.)) 과 동일
+# print(x.grad)
+# print(x)
+# print(y)
+# print(z)
+
+
