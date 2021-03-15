553b2358b7fe0cfabf3b44716c013c0c308c9588
JaewonMin <mjw8523@gmail.com>
2021-03-16 00:40:17 +0900

    3/16

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-import torch
+from glob import glob
+import os
+from tqdm.notebook import tqdm
+import shutil
 
-x = torch.ones(2,2, requires_grad = True)
-y = x + 2
+save = './total'
+img_path='./APY170401435_subset_13gb/'
 
-z = y*y*3
+session = 'session01'
+folder_list = ['G{:04d}'.format(i) for i in range(1,101+1)]
 
-out = z.mean()
-print(out)
-out.backward()	# out.backward(torch.tensor(1.)) 과 동일
-# print(x.grad)
-# print(x)
-# print(y)
-# print(z)
+# if not os.path.exists(save):
+#     os.mkdir(save)
+
+for folder_name in folder_list:
+    src = os.path.join(img_path,folder_name,session)
+    jpg_list = sorted(glob(src+'/*.jpg'))
+    metadata_list = sorted(glob(src+'/*.metadata'))
+    txt_list = sorted(glob(src+'/*.txt'))
+    print(img_path+'/'+folder_name+'/'+session)
+    print(folder_name)
+
+
+    
+
+
 
 
