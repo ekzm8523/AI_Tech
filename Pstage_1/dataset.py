cea7808024fce0a47a9c89fa8a9ebe81d332ee19
JaewonMin <mjw8523@gmail.com>
2021-04-03 13:53:22 +0900

    4/3 commit

---
+++ Pstage_1/dataset.py
@@ -0,0 +1,179 @@
+import os
+import random
+from collections import defaultdict
+
+import numpy as np
+import torch
+import torch.utils.data as data
+from PIL import Image
+
+IMG_EXTENSIONS = [
+    ".jpg", ".JPG", ".jpeg", ".JPEG", ".png"
+    ".PNG", ".ppm", ".PPM", ".bmp", ".BMP"
+]
+
+def is_image_file(filename):
+    return any(filename.endswitch(extension) for extension in IMG_EXTENSIONS)
+
+
+
+class MaskBaseDataset(data.Dataset):
+    num_classes = 3 * 2 * 3
+
+    class MaskLabels:
+        maks = 0
+        incorrect = 1
+        normal = 2
+
+    class GenderLabels:
+        male = 0
+        female = 1
+
+    class AgeGroup:
+        map_label = lambda x: 0 if int(x) < 30 else 1 if int(x) < 60 else 2
+
+    _file_names = {
+        "mask1": MaskLabels.maks,
+        "mask2": MaskLabels.maks,
+        "mask3": MaskLabels.maks,
+        "mask4": MaskLabels.maks,
+        "mask5": MaskLabels.maks,
+        "incorrect_mask": MaskLabels.incorrect,
+        "normal": MaskLabels.normal
+    }
+
+    image_paths = []
+    mask_labels = []
+    gender_labels = []
+    age_labels = []
+
+    def __init__(self, data_dir, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), val_ratio=0.2):
+        self.data_dir = data_dir
+        self.mean = mean
+        self.std = std
+        self.val_ratio = val_ratio
+
+        self.transform = None
+        self.setup()
+        self.calc_statistics()
+
+    def setup(self):
+        profiles = os.listdir(self.data_dir)
+        for profile in profiles:
+            if profile.startswith("."): # "." 로 시작하는 파일은 무시합니다.
+                continue
+
+            img_folder = os.path.join(self.data_dir, profile)
+            for file_name in os.listdir(img_folder):
+                _file_name, ext = os.path.splitext(file_name)
+                if _file_name not in self._file_names: # invalid 한 파일들은 무시합니다.
+                    continue
+                img_path = os.path.join(self.data_dir, profile, file_name) # (resized_data, 000004_male_Asian_54, mask1.jpg)
+                mask_label = self._file_names[_file_name]
+
+                id, gender, race, age = profile.split("_")
+                gender_label = getattr(self.GenderLabels, gender) # self.GenderLables.gender
+                age_label = self.AgeGroup.map_label(age)
+
+                self.image_paths.append(img_path)
+                self.mask_labels.append(mask_label)
+                self.gender_labels.append(gender_label)
+                self.age_labels.append(age_label)
+
+
+    def calc_statistics(self):
+        has_statistics = self.mean is not None and self.std is not None
+        if not has_statistics:
+            print("[Warning] Calculating statistics... It can takes huge amounts of time depending on your CPU mashine :(")
+            sums = []
+            squared = []
+            for image_path in self.image_paths[:3000]:
+                image = np.array(Image.open(image_path)).astype(np.int32)
+                sums.append(image.mean(axis=(0, 1)))
+                squared.append((image ** 2).mean(axis=(0, 1)))
+
+            self.mean = np.mean(sums, axis=0) / 255
+            self.std = (np.mean(squared, axis=0) - self.mean ** 2) ** 0.5 / 255
+
+    def set_transform(self, transform):
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # image = self.read_image(index)
+        pass
+    def __len__(self):
+        return len(self.image_paths)
+
+    def get_mask_label(self, index):
+        return self.mask_labels[index]
+
+    def get_gender_label(self, index):
+        return self.gender_labels[index]
+
+    def get_age_label(self, index):
+        return self.age_labels[index]
+
+    def read_image(self, index):
+        image_path = self.image_paths[index]
+        return Image.open(image_path)
+
+    @staticmethod
+    def encode_multi_class(mask_label, gender_label, age_label):
+        return mask_label * 6 + gender_label * 3 + age_label
+
+    @staticmethod
+    def decode_multi_class(multi_class_label):
+        mask_label = (multi_class_label // 6) % 3
+        gender_label = (multi_class_label // 3) % 2
+        age_label = multi_class_label % 3
+        return mask_label, gender_label, age_label
+
+    @staticmethod
+    def denormalize_image(image, mean, std):
+        img_cp = image.copy()
+        img_cp *= std
+        img_cp += mean
+        img_cp *= 255.0
+        img_cp = np.clip(img_cp, 0, 255).astype(np.uint8)
+
+    def split_dataset(self):
+        n_val = int(len(self) * self.val_ratio)
+        n_train = len(self) - n_val
+        train_set, val_set = torch.utils.data.random_split(self, [n_train, n_val])
+        return train_set, val_set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
