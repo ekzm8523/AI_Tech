22818115fa831e63f0b7b4edfb02e3dd84f6766d
JaewonMin <mjw8523@gmail.com>
2021-04-04 23:37:54 +0900

    4/4 inference complete

--- Pstage_1/dataset.py
+++ Pstage_1/dataset.py
@@ -202,3 +202,21 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
     def split_dataset(self):
         return [Subset(self, indices) for phase, indices in self.indices.items()]
 
+class TestDataset(data.Dataset):
+    def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
+        self.img_paths = img_paths
+        self.transform = transforms.Compose([
+            Resize(resize, Image.BILINEAR),
+            ToTensor(),
+            Normalize(mean=mean, std=std),
+        ])
+
+    def __getitem__(self, index):
+        image = Image.open(self.img_paths[index])
+
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+    def __len__(self):
+        return len(self.img_paths)
