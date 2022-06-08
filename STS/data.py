080a531395a5917337c083dd27ec7a9fdce94d02
ekzm8523 <mjw8523@gmail.com>
2022-06-08 16:01:33 +0900

    dataset class

--- STS/data.py
+++ STS/data.py
@@ -9,7 +9,7 @@ class KLUEDataset:
     dataset: Optional[DatasetDict]
 
     def __init__(self, task: str):
-        dataset = load_dataset('klue', task).flatten().rename_column('labels.real-label', 'label')
+        dataset = load_dataset('klue', task)
         dataset = dataset.flatten()  # KLUE는 label이 labels로 되어있어 real-label, binary-label, label로 이루어져있음
         dataset = dataset.rename_column('labels.real-label', 'label')
         self.dataset = dataset.remove_columns(['labels.label', 'labels.binary-label'])
